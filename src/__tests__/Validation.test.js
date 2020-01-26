d84335add92511df2f95996c1c3d041dd7de92a4
Clyde D'Souza <clyd94@gmail.com>
2020-01-27 01:00:59 +1300

    Move test files into new tests folder and add validation tests

---
+++ src/__tests__/Validation.test.js
@@ -0,0 +1,19 @@
+import {Calculation} from '../utils/Calculation';
+const calculation = new Calculation();
+
+it('Valid inputs', () => {
+    expect(calculation.IsValid(200)).toBe(true);
+    expect(calculation.IsValid(35.7)).toBe(true);
+    expect(calculation.IsValid(1)).toBe(true);
+});
+
+it('Invalid inputs', () => {
+    expect(calculation.IsValid(0)).toBe(false);
+    expect(calculation.IsValid(-5)).toBe(false);
+    expect(calculation.IsValid("")).toBe(false);
+    expect(calculation.IsValid(" ")).toBe(false);
+    expect(calculation.IsValid("abc")).toBe(false);
+    expect(calculation.IsValid("y")).toBe(false);
+    expect(calculation.IsValid(null)).toBe(false);
+    expect(calculation.IsValid(undefined)).toBe(false); 
+});
